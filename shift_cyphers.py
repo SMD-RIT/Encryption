49443f414d027ae5056c3c99245addc9a93d3565
Sean Dunn <smd6336@rit.edu>
2020-08-26 01:44:02 -0400

    First commit

---
+++ shift_cyphers.py
@@ -0,0 +1,24 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def charToNum(c, a):
+    return a.index(c) + 1
+
+def numToChar(n, a):
+    return a[n-1]
+
+def encryptCaesar():
+    return ""
+
+def decryptCaesar():
+    return ""
+
+def encryptAffine():
+    return ""
+
+def decryptAffine():
+    return ""
+
+def main():
+    print("Crypto")
+
+main()
